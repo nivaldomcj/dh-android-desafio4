90cf8a2d1eb9eb0872ad68f4f7c5cc0fb635555c
Nivaldo Carvalho <nivaldo_j@outlook.com>
2021-02-15 11:14:09 -0300

    Added 'sign out' button (layout) to HomeFragment

--- app/src/main/java/nivaldo/dh/exercise/firebase/home/view/HomeFragment.kt
+++ app/src/main/java/nivaldo/dh/exercise/firebase/home/view/HomeFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import nivaldo.dh.exercise.firebase.databinding.FragmentHomeBinding
 import nivaldo.dh.exercise.firebase.home.model.Game
@@ -47,6 +47,9 @@ class HomeFragment : Fragment() {
 
     private fun initComponents() {
         binding.fabCreateGame.setOnClickListener {
+            // TODO
+        }
+        binding.btnSignOut.setOnClickListener {
             // TODO
         }
     }
